c726afc81f4ca57282cb1cf863551891ce3432ce
Giorgos Flouris <giorgos.flouris.98@gmail.com>
2020-02-24 11:27:10 +0200

    Added list characteristics

--- AlphaToken.h
+++ AlphaToken.h
@@ -1,9 +1,20 @@
+#include <assert.h>
+#include <stdlib.h>
+#include <stdio.h>
+
 struct AlphaToken{
     
     int line;
     char* type;
-    char* value;
+    char* value;    
 
-    AlphaToken* next;
+    struct AlphaToken* next;
 
 };
+
+struct AlphaToken * tokenListHead = NULL;
+
+void alphaListInsert(int line, char* value, char* type);
+
+void printAlphaList();
+
