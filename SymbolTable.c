2887b38fab250adbf6841e84019eb91998291c79
Giorgos Flouris <giorgos.flouris.98@gmail.com>
2020-03-22 16:40:15 +0200

    Lookup implementation added

--- SymbolTable.c
+++ SymbolTable.c
@@ -4,7 +4,6 @@
 SymbolTableEntry *SymbolTable[1034];
 
 
-
 int hashForBucket(char *symbolName){
     assert(symbolName != NULL);
     return (atoi(symbolName) * HASH_NUMBER) % NON_SCOPE_BUCKETS;
@@ -21,12 +20,78 @@ void insertEntry(SymbolTableEntry *symbol){
 
 
 SymbolTableEntry *lookupEverything(char *name){
+    int bucket;
+    SymbolTableEntry *symbolIndex;
+    Variable *varTMP;
+    Function *funcTMP;
 
+    assert(name != NULL);
+
+    bucket = hashForBucket(name);
+    if(SymbolTable[bucket] == NULL){
+        return NULL;
+    }
+
+    symbolIndex = SymbolTable[bucket] -> next;
+    assert(symbolIndex != NULL);
+
+    while(symbolIndex != NULL){
+        
+        if(symbolIndex -> value.varVal != NULL){
+            varTMP = symbolIndex -> value.varVal;
+            if(strcmp(varTMP -> name, name) == 0){
+                return symbolIndex;
+            }
+        }
+
+        if(symbolIndex -> value.funcVal != NULL){
+            funcTMP = symbolIndex -> value.funcVal;
+            if(strcmp(funcTMP -> name, name) == 0){
+                return symbolIndex;
+            }
+        }
+        symbolIndex = symbolIndex -> next;
+    }
+
+    return NULL;
 }
 
 
 SymbolTableEntry *lookupScope(char *name, int scope){
+    int bucket;
+    SymbolTableEntry *symbolIndex;
+    Variable *varTMP;
+    Function *funcTMP;
 
+    assert(name != NULL);
+
+    bucket = hashForScope(scope);
+    if(SymbolTable[bucket] == NULL){
+        return NULL;
+    }
+
+    symbolIndex = SymbolTable[bucket] -> next;
+    assert(symbolIndex != NULL);
+
+    while(symbolIndex != NULL){
+        
+        if(symbolIndex -> value.varVal != NULL){
+            varTMP = symbolIndex -> value.varVal;
+            if(strcmp(varTMP -> name, name) == 0){
+                return symbolIndex;
+            }
+        }
+
+        if(symbolIndex -> value.funcVal != NULL){
+            funcTMP = symbolIndex -> value.funcVal;
+            if(strcmp(funcTMP -> name, name) == 0){
+                return symbolIndex;
+            }
+        }
+        symbolIndex = symbolIndex -> next;
+    }
+
+    return NULL;
 }
 
 
